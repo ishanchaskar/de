ca0dbc08dc6ed6c7879a9e0222ef6b8b4a428220
ishanchaskar <91770647+ishanchaskar@users.noreply.github.com>
2025-03-26 15:12:04 +0530

    wwwwww

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --host=0.0.0.0 --port=$PORT App:app
+web: gunicorn --bind 0.0.0.0:10000 App:app
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gdown --id 1WeSZoGTIvp1qkhuAxIiDmSQVIYoWIoXb -O asl_16feb.h5
-gunicorn --bind 0.0.0.0:10000 app:app
+gunicorn --bind 0.0.0.0:10000 App:app
